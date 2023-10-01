3e2b0cded0200ea36635addda6b7d846613d6edb
swethapasupuleti-git <141996206+swethapasupuleti-git@users.noreply.github.com>
2023-10-01 12:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 var team = players;
-	var team1 = Array.form(players);
-	var cap1 = Array.form(person);
+	var team1 = [...players];
+	var cap1 = [...person];
 window.players = players;
 window.person = person;
 window.team = team;
